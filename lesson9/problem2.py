2c588b43c3a4bb57218b7942acd1930fb410dc20
kailaaa <44173171+kailaaa@users.noreply.github.com>
2018-12-20 13:23:40 -0500

    first commit

---
+++ lesson9/problem2.py
@@ -0,0 +1,10 @@
+print('-'*60)
+print('Allien Greeter Bot: ')
+print()
+password = input('What planet are you from? ')
+if password == 'Mars' or password == 'mars':
+	print('Welcome to Earth, please do not kill us! ')
+else:
+	print('Dammmmnnnnn, your face is ugly I thought you were an alien.')
+	print('Anyway...have a good day! ')
+print('-'*60)
